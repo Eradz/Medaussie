ee4db95f2151c646b46a17cd7af077fc5997bf3f
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-11-08 17:01:19 +0100

    added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-// import localFont from "next/font/local";
 import {Poppins} from "next/font/google"
 import "./globals.css";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 const poppins = Poppins({
-  weight: "500",
+  weight: "400",
   style: "normal",
   subsets: ["latin"]
 })
@@ -31,7 +20,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         className={poppins.className}
       >
         {children}
