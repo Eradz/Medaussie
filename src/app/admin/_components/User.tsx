f7a3d12141e862ed14869c787f283b26f98f05b5
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-12-03 14:07:26 +0100

    Added user admin page

--- src/app/admin/_components/User.tsx
+++ src/app/admin/_components/User.tsx
@@ -1,46 +1,51 @@
 import React from 'react'
 
 const users :{firstname: string, lastname: string, email: string, role: string}[] = [
-    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'user'},
+    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'User'},
+    {firstname: 'Chidiebereeeeeeee', lastname: 'Anagu', email: 'anaguchidiebere@example.com', role: 'Admin'},
+    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'User'},
+    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'User'},
     {firstname: 'Chidiebere', lastname: 'Anagu', email: 'anaguchidiebere@example.com', role: 'Admin'},
-    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'user'},
-    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'user'},
+    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'User'},
+    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'User'},
     {firstname: 'Chidiebere', lastname: 'Anagu', email: 'anaguchidiebere@example.com', role: 'Admin'},
-    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'user'},
-    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'user'},
+    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'User'},
+    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'User'},
     {firstname: 'Chidiebere', lastname: 'Anagu', email: 'anaguchidiebere@example.com', role: 'Admin'},
-    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'user'},
-    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'user'},
+    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'User'},
+    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'User'},
     {firstname: 'Chidiebere', lastname: 'Anagu', email: 'anaguchidiebere@example.com', role: 'Admin'},
-    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'user'},
-    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'user'},
-    {firstname: 'Chidiebere', lastname: 'Anagu', email: 'anaguchidiebere@example.com', role: 'Admin'},
-    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'user'},
-    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'user'},
+    {firstname: 'Uche', lastname: 'Anagu', email: 'Uche@example.com', role: 'User'},
+    {firstname: 'John', lastname: 'Doe', email: 'John@example.com', role: 'User'},
 ]
 const User = () => {
   return (
- <div className='bg-primary h-screen'>
-    <div className='flex py-3 px-[5%]'>
-        <p className='w-[15%]'>First Name</p>
-        <p className='w-[15%]'>Last Name</p>
-        <p className='w-[35%]'>Email</p>
-        <p className='w-[10%]'>Role</p>
-        <p className='w-[10%]'>Edit</p>
-        <p className='w-[10%]'>Delete</p>
+ <div className='bg-primary h-full overflow-scroll overflow-x-hidden flex flex-col '>
+    <table className=' flex flex-col border-collapse'>
+    <div className='flex border-collapse'>
+        <p className='w-[5%] border border-solid border-slate-400 py-1 pl-[6px]'>S/N</p>
+        <p className='w-[15%] border border-solid border-slate-400 py-1 pl-[6px] '>First Name</p>
+        <p className='w-[15%] border border-solid border-slate-400 py-1 pl-[6px]'>Last Name</p>
+        <p className='w-[35%] border border-solid border-slate-400 py-1 pl-[6px] '>Email</p>
+        <p className='w-[10%] border border-solid border-slate-400 py-1 pl-[6px]'>Role</p>
+        <p className='w-[10%] border border-solid border-slate-400 py-1 pl-[6px]'>Edit</p>
+        <p className='w-[10%] border border-solid border-slate-400 py-1 pl-[6px]'>Delete</p>
     </div>
   {users.map((user, i) => {
     return (
-    <div key={i} className='py-3 px-[5%] flex'>
-        <p className='w-[15%]'>{user.firstname}</p>
-        <p className='w-[15%]'>{user.lastname}</p>
-        <p className='w-[35%]'>{user.email}</p>
-        <p className='w-[10%]'>{user.role}</p>
-        <div className='w-[10%] px-1  text-center'><p className='bg-green-500 p-1 rounded-lg'>Edit</p></div>
-        <div className='w-[10%] px-1 text-center'><p className='bg-red-500 p-1 rounded-lg'>Delete</p></div>
-    </div>
+    <tbody key={i} className={i % 2 != 0 ? 'flex items-center' : 'flex items-center bg-[#dddddd] '}>
+        <tr className='w-[5%] py-1 pl-[6px] '>{i + 1}</tr>
+        <tr className='w-[15%] py-1 pl-[6px] overflow-hidden'>{user.firstname}</tr>
+        <tr className='w-[15%] py-1 pl-[6px] overflow-hidden'>{user.lastname}</tr>
+        <tr className='w-[35%] py-1 pl-[6px] overflow-hidden'>{user.email}</tr>
+        <tr className='w-[10%] py-1 pl-[6px]'>{user.role}</tr>
+        <tr className='w-[10%] py-1 pl-[6px] text-center hover:cursor-pointer bg-green-500 hover:bg-green-300'>Edit</tr>
+        <tr className='w-[10%] py-1 pl-[6px] text-center hover:cursor-pointer bg-red-500 hover:bg-red-200'>Delete</tr>
+    </tbody>
 )
-})}     
+})}   
+</table>
+
 </div>
   )
 }
