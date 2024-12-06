f3a2c5a5196960799db5c5483290f04368b97306
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-12-06 20:27:29 +0100

    added toolbar menu, Working on image upload

--- src/components/tiptap/toolbar.tsx
+++ src/components/tiptap/toolbar.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react'
 import { type Editor } from '@tiptap/react'
-import {Bold, Strikethrough, Italic, List, ListOrdered, Heading2, Underline, Quote, Undo, Redo, Code} from "lucide-react"
+import {Bold, Strikethrough, Italic, List, ListOrdered, Heading2, Underline, Quote, Undo, Redo, Image} from "lucide-react"
 const Toolbar = ({editor, content}: {editor: Editor | null, content: string }) => {
 
     if(!editor){
@@ -9,31 +9,37 @@ const Toolbar = ({editor, content}: {editor: Editor | null, content: string }) =
     }
     const toolBarElements = [
         {icon: <Bold/>, click: ()=>editor.chain().focus().toggleBold().run(), name: "bold"},
-        {icon: <Strikethrough/>, click: ()=>editor.chain().focus().toggleStrike().run(), name: "StrikeThrough"},
-        {icon: <Italic/>, click: ()=>editor.chain().focus().toggleItalic().run(), name: "Itaic"},
-        {icon: <List/>, click: ()=>editor.chain().focus().toggleBulletList().run(), name: "List"},
-        {icon: <ListOrdered/>, click: ()=>editor.chain().focus().toggleOrderedList().run(), name: "ListOrdered"},
-        {icon: <Heading2/>, click: ()=>editor.chain().focus().toggleHeading({level: 1}).run(), name: "Heading2"},
-        {icon: <Underline/>, click: ()=>editor.chain().focus().toggleUnderline().run(), name: "Underline"},
-        {icon: <Quote/>, click: ()=>editor.chain().focus().toggleBulletList().run(), name: "quote"},
-        {icon: <Undo/>, click: ()=>editor.chain().focus().toggleStrike().run(), name: "Undo"},
-        {icon: <Redo/>, click: ()=>editor.chain().focus().toggleItalic().run(), name: "Redo"},
-        {icon: <Code/>, click: ()=>editor.chain().focus().toggleBulletList().run(), name: "Code"},
+        {icon: <Strikethrough/>, click: ()=>editor.chain().focus().toggleStrike().run(), name: "strike"},
+        {icon: <Italic/>, click: ()=>editor.chain().focus().toggleItalic().run(), name: "italic"},
+        {icon: <List/>, click: ()=>editor.chain().focus().toggleBulletList().run(), name: "bulletList"},
+        {icon: <ListOrdered/>, click: ()=>editor.chain().focus().toggleOrderedList().run(), name: "orderedList"},
+        {icon: <Heading2/>, click: ()=>editor.chain().focus().toggleHeading({level: 2}).run(), name: "heading2"},
+        {icon: <Underline/>, click: ()=>editor.chain().focus().toggleUnderline().run(), name: "underline"},
+        {icon: <Quote/>, click: ()=>editor.chain().focus().toggleBlockquote().run(), name: "blockquote"},
+        {icon: <Undo/>, click: ()=>editor.chain().focus().undo().run(), name: "undo"},
+        {icon: <Redo/>, click: ()=>editor.chain().focus().redo().run(), name: "redo"},
+        {icon: <Image/>, click: ()=>editor.chain().focus().toggleBulletList().run(), name: "image"},
     ]
   return (
-    <div className='w-full h-8 border border-solid border-secondary'>
-       <div className='w-[50%] flex items-end justify-between'>
+    <div className='w-full h-16 border border-solid border-secondary justify-between flex'>
+        
+       <div className='w-[50%] h-full flex gap-2 p-2'>
         {toolBarElements.map(({name, icon, click}) => {
             return (
-                <button key={name} onClick={(e)=>{
-                    e.preventDefault();
-                    click()
-                    }}>
+                <button key={name} 
+                    onClick={(e)=>{
+                        e.preventDefault();
+                        click()
+                        }}
+                    className={editor.isActive(name) ? 'bg-secondary text-primary p-2 rounded-lg' : 'text-secondary p-2'}
+                        >
                     {icon}
                 </button>
             )
         })}
        </div>
+
+        {content.length > 7 && <button type='submit' className='py-2 px-4 bg-secondary text-primary font-bold' >Add</button>}
     </div>
   )
 }
