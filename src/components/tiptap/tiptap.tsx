5c1a0f2f0a3e5ccdfc375a3b59772bb220524a60
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-12-07 21:45:21 +0100

    Added image upload

--- src/components/tiptap/tiptap.tsx
+++ src/components/tiptap/tiptap.tsx
@@ -3,11 +3,12 @@
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import Underline from '@tiptap/extension-underline'
+import Image from '@tiptap/extension-image'
 import Toolbar from './toolbar'
 
 const Tiptap = ({onchange, content}: {onchange: (string: string)=> void, content: string}) => {
   const editor = useEditor({
-    extensions: [Underline, StarterKit],
+    extensions: [Underline, StarterKit, Image],
     editorProps:{
       attributes:{
         class: "prose max-w-none [&_ol]:list-decimal [&_ul]:list-disc border-b border-l border-r px-4 pt-3 pb-[100px] outline-none border-secondary",
--- src/components/tiptap/toolbar.tsx
+++ src/components/tiptap/toolbar.tsx
@@ -1,17 +1,37 @@
 "use client"
-// import React, {useCallback} from 'react'
 import { type Editor } from '@tiptap/react'
-// import { callApi } from '@zayne-labs/callapi'
-import {Bold, Strikethrough, Italic, List, ListOrdered, Heading2, Underline, Undo, Redo} from "lucide-react"
+import {Bold, Strikethrough, Italic, List, ListOrdered, Heading2, Underline, Undo, Redo, ImageIcon} from "lucide-react"
+import { callApi } from '@zayne-labs/callapi'
+import { toast } from 'sonner'
 const Toolbar = ({editor, content}: {editor: Editor | null, content: string }) => {
-
+    
+    const handleImageUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const target = event.target.files?.[0]
+        if (!target) return;
+    
+        const formData = new FormData();
+        formData.append('file', target);
+    
+        // Upload the image to your server
+        await callApi<{message: string, data: string}>(process.env.NEXT_PUBLIC_NEXT_ENV === "development" ? 'http://localhost:5000/api/v1/upload/single' : "https://medaussie-backend.onrender.com/api/v1/upload/single", {
+          method: 'POST',
+          body: formData,
+          onResponse: ({ data }) => {
+            editor?.chain().focus().setImage({ src: data.data }).run();
+          },
+          onError: ({ error }) => {
+            toast.error(error.message)
+            console.log(error.message)
+          },
+        })
+      };
     if(!editor){
         return false
     }
-
-    // const uploadImage = useCallback(async() => {
-    //     const url = await callApi()
-    // }, [editor])
+    const triggerFileInput = () => {
+            const fileInput = document.getElementById('file-input') as HTMLInputElement;
+            fileInput?.click();
+      };
     const toolBarElements = [
         {icon: <Bold/>, click: ()=>editor.chain().focus().toggleBold().run(), name: "bold"},
         {icon: <Strikethrough/>, click: ()=>editor.chain().focus().toggleStrike().run(), name: "strike"},
@@ -22,8 +42,10 @@ const Toolbar = ({editor, content}: {editor: Editor | null, content: string }) =
         {icon: <Underline/>, click: ()=>editor.chain().focus().toggleUnderline().run(), name: "underline"},
         {icon: <Undo/>, click: ()=>editor.chain().focus().undo().run(), name: "undo"},
         {icon: <Redo/>, click: ()=>editor.chain().focus().redo().run(), name: "redo"},
-        // {icon: <Image/>, click: ()=>editor.chain().focus().toggleBulletList().run(), name: "image"},
+        {icon: <ImageIcon/>, click: () => triggerFileInput(), name: "image"},
     ]
+
+   
   return (
     <div className='w-full h-16 border border-solid border-secondary justify-between flex'>
         
@@ -38,9 +60,10 @@ const Toolbar = ({editor, content}: {editor: Editor | null, content: string }) =
                     className={editor.isActive(name) ? 'bg-secondary text-primary p-2 rounded-lg' : 'text-secondary p-2'}
                         >
                     {icon}
-                </button>
+                </button>  
             )
         })}
+        <input id='file-input' type="file" onChange={handleImageUpload} className='hidden'/>
        </div>
 
         {content.length > 7 && <button type='submit' className='py-2 px-4 bg-secondary text-primary font-bold' >Add</button>}
