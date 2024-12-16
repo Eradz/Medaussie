c9109ecfe8c03e14eab794ae386d320d931da540
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2024-12-16 21:00:07 +0100

    fixed async promise params

--- src/app/admin/exams/[...slug]/page.tsx
+++ src/app/admin/exams/[...slug]/page.tsx
@@ -1,5 +1,5 @@
 import EditExam from "../../_components/edit/exam"
-const Page = async ({params} : {params: { slug: string }}) => {
+const Page = async ({params} : {params: Promise<{ slug: string }>}) => {
 
   const slug : string = (await params).slug
    return(
